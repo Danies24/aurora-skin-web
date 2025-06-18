a5eda2040cb58da9b5aa7484ce09cd2297804f25
Danies-CDM <danies@codingmart.com>
2025-06-18 16:06:56 +0530

    Product Listing page has been checked

--- herbaurora/src/app/product/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductDetailsPage from "@/pages/ProductDetailsPage";
-
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetailsPage id={params.id} />;
-}
